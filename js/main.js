5331c8d3d6e90c6b30975be80b3819dbbcc64220
FCubilla <facu_10.98@hotmail.com>
2026-08-25 10:58:51 -0300

    Mi primer commit de registro de jugadores

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const nombre = prompt("Ingrese su nombre");
+
+const apellido = prompt("Ingrese su apellido");
+
+let edad = Number(prompt("Ingrese su edad"));
+
+let telefono = prompt("Ingrese su telefono");
+
+let categoria = prompt("Por ultimo ingrese su categoria ");
+
+let añoDeNacimiento = 2026 - edad;
+
+alert("Hola " + nombre + ". Gracias por registrarte en nuestra pagina de padel!!, tu apellido es " + apellido + ", tu telefono es " + telefono + ", tu año de nacimiento es " + añoDeNacimiento + " y tu categoria es " + categoria);
+
+console.log(confirm("Esta conforme con los datos ingresados en su Ficha de Jugador de Padel?"));
